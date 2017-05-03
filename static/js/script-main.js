e55bb1ae6951d9905f703d07397368454a622c4c
meganduffy <meganemilyduffy@gmail.com>
2017-05-03 21:54:56 +0100

    Removed container from about, styled about blue, added shadow to navbar, attempted to add js functionality to about-2 section (not currently working), added icons to services

--- static/js/script-main.js
+++ static/js/script-main.js
@@ -4,3 +4,41 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+// // function ShowTATInfo() {
+// //   if ($(".mydivclass")[0]){
+// //     $(this).removeClass
+// // } else {
+// //     // Do something if class does not exist
+// // }
+// //   document.getElementById("TATInfo").style.display = "block";
+// // }
+// // // function ShowTimeStampsInfo() {
+// // //   document.getElementById("TimeStampsInfo").style.display = "block";
+// // // }
+// //
+//
+//
+// $('.about-2-icon-box').click(function() {
+//   var $t = $(this);
+//
+// });
+
+// TODO: When .about-2-icon is clicked, show information tabs
+
+    var tabFinish = 0;
+
+    $('.about-2-icon-box').click(function () {
+        var $t = $(this);
+        if (tabFinish || $t.hasClass('active')) return false;
+        tabFinish = 1;
+        $t.closest('.about-2').find('.about-2-icon-box').removeClass('active-item');
+        $t.addClass('.active-item');
+        var index = $t.parent().find('.about-icon-2-box').index(this);
+        $t.closest('.about-2').find('.about-description:visible').fadeOut(500, function () {
+            $t.closest('.about-2').find('.about-description').eq(index).fadeIn(500,
+            function () {
+                tabFinish = 0;
+            })
+        })
+    });
